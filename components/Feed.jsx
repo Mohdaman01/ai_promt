49b785c9fe240333c10c3a69218bab322048cfcc
Mohdaman01 <mohdastar200026@gmail.com>
2024-02-06 23:58:05 +0530

    pushing to github

---
+++ components/Feed.jsx
@@ -0,0 +1,105 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+
+import PromptCard from './PromtCard';
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  )
+}
+
+const Feed = () => {
+
+  const [posts, setPosts] = useState([]);
+  const [searchText, setSearchText] = useState('');
+
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+
+      const response = await fetch('/api/prompt');
+
+      const data = await response.json();
+
+      setPosts(data);
+
+    })();
+
+  }, []);
+
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
+    return posts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
+
+
+  const handleSearchChange = (e) => {
+
+    e.preventDefault();
+
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
+
+  return (
+    <section className='feed' >
+      <form className='relative w-full flex-center' onSubmit={(e) => e.preventDefault()} >
+        <input
+          type="text"
+          placeholder='Search for a tag or a username'
+          value={searchText}
+          onChange={(e) => handleSearchChange(e)}
+          required
+          className='search_input peer'
+        />
+      </form>
+
+
+      {/* All Prompts */}
+      {searchText ? (
+        <PromptCardList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardList data={posts} handleTagClick={handleTagClick} />
+      )}
+
+    </section>
+  )
+}
+
+export default Feed
